4f2e8bb21486b047fca2c88c577882ef1771becb
KarenStephy <k.musangwa@alustudent.com>
2026-02-08 13:24:45 -0800

    Hello

---
+++ vault_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#create directory
+mkdir secure_vault
+
+touch secure_vault
+touch ./secure_vault/keys.txt ./secure_vault/secrets.txt ./secure_vault/logs.txt
+echo "Welcome to keys file" >./secure_vault/keys.txt 
+echo "Welcome to secrets file" >./secure_vault/secrets.txt
+echo "Welcome to logs files" >./secure_vault/logs.txt
+echo "files successfully created"
+ls -l secure_vault
+
